bce7965aa62e4ac414ba5f47c6a781cfe5daaa83
iri6ka <ikonnova@gmail.com>
2021-05-30 11:32:09 +1000

    saved last changes for gradient generator react app"

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import { Component } from 'react'
+import React, { Component, useEffect, useState } from 'react'
 import styled from 'styled-components'
 
 import './App.css'
 
 const StyledMain = styled.main`
-  background-color: ${props => props.backGroundColor[0]};
+  ${props => props.backgroundCSS}
   font-family: 'Courier New', Courier, monospace;
   display: flex;
   flex-direction: column;
@@ -13,37 +13,133 @@ const StyledMain = styled.main`
   width: 100%;
 `
 
-class App extends Component {
-  state = {
-    backGroundColor: ["#ffffff", "#ffffff", "#ffffff"]
-  }
+// background: linear-gradient(${props => props.backGroundColor[0]}); */
+// background: linear-gradient(#fff, #000, #fff);
 
-  handleColorChange = (event, i) => {
+// takes in an array of background colors, and returns css for a gradient colors
+export const generateBackgroundCSS = (backgroundColors) => {
+  // loop over the background colors, add each one to the string
+  let output = "background: linear-gradient("
+  // backgroundColors.forEach((color) => output += `${color},`)
+  output += backgroundColors.join(', ')
+  output += ");"
+  return output
+}
+
+const updateLocalStorage = (state) => {
+  //JSON strignify the state
+  const stringState = JSON.stringify(state)
+  // save string to local storage
+  localStorage.setItem('backgroundColors', stringState)
+
+}
+const getFromLocalStorage = () => {
+  const colors = localStorage.getItem('backgroundColors')
+  if (!colors) {
+    return null
+  }
+  return JSON.parse(colors)
+}
+
+const App = () => {
+  const [backgroundColors, setBackgroundColors] = useState(["#ffffff", "#ffffff", "#ffffff"])
+  useEffect(() => {
+    const colors = getFromLocalStorage()
+    if (colors) {
+      setBackgroundColors(colors)
+    }
+  }, [])
+
+  useEffect(() => {
+    updateLocalStorage(backgroundColors)
+  },[backgroundColors])
+  
+  const handleColorChange = (event, i) => {
     const newColor = event.target.value
     // create a copy of the backGroundColor array
-    const newBackgroundColor = [...this.state.backGroundColor]
+    const newBackgroundColors = [...backgroundColors]
     // change the value of the array to the new color
-    newBackgroundColor[i] = newColor
+    newBackgroundColors[i] = newColor
     // set the new array as the new state
-    this.setState({ backGroundColor: newBackgroundColor })
+    // this.setState({ backGroundColor: newBackgroundColor })
+    setBackgroundColors(newBackgroundColors)
   }
 
-  render() {
-    const { backGroundColor } = this.state
+  const addColor = () => {
+    // clone the existing colors array
+    const newBackgroundColors = [...backgroundColors]
+    // add the color to the end
+    newBackgroundColors.push("#ffffff")
+    // set it into the state (change array without mutating)
+    setBackgroundColors(newBackgroundColors)
+  }
 
-    return (
-      // Pass the chosen color into main to set the background
-      <StyledMain backGroundColor={backGroundColor}>
-        <h1>CSS Gradient Generator</h1>
-        {backGroundColor.map((color, i) => (
-          <input type="color"
-            value={color}
-            onChange={(event) => this.handleColorChange(event, i)}
+  const removeColor = (i) => {
+    // clone background color array
+    const newBackgroundColors = [...backgroundColors]
+    // remove the element index i
+    newBackgroundColors.splice(i,1)
+    // set new array into state
+    setBackgroundColors(newBackgroundColors)  
+
+  }
+
+  const backgroundCSS = generateBackgroundCSS(backgroundColors)
+  return (
+    // Pass the chosen color into main to set the background
+    <StyledMain backgroundCSS = {backgroundCSS}>
+      <h1>CSS Gradient Generator</h1>
+      {backgroundColors.map((color, i) => (
+        <React.Fragment key={`${i}${color}`}>
+        <input  type="color"
+          value={color}
+          onChange={(event) => handleColorChange(event, i)}
           />
-        ))}
-      </StyledMain>
-    )
-  }
+          {backgroundColors.length > 2 && <button onClick={() => removeColor(i)}>Remove</button>}
+          </React.Fragment>
+      ))}
+      <button onClick={addColor}>Add a color</button> 
+      <p>
+        Your CSS is:
+      </p>
+      <h5>
+      {backgroundCSS}
+      </h5>
+    </StyledMain>
+  )
 }
+
+// class OldApp extends Component {
+//   state = {
+//     backGroundColor: ["#ffffff", "#ffffff", "#ffffff"]
+//   }
+
+//   handleColorChange = (event, i) => {
+//     const newColor = event.target.value
+//     // create a copy of the backGroundColor array
+//     const newBackgroundColor = [...this.state.backGroundColor]
+//     // change the value of the array to the new color
+//     newBackgroundColor[i] = newColor
+//     // set the new array as the new state
+//     this.setState({ backGroundColor: newBackgroundColor })
+//   }
+
+//   render() {
+//     const { backGroundColor } = this.state
+
+//     return (
+//       // Pass the chosen color into main to set the background
+//       <StyledMain backGroundColor={backGroundColor}>
+//         <h1>CSS Gradient Generator</h1>
+//         {backGroundColor.map((color, i) => (
+//           <input type="color"
+//             value={color}
+//             onChange={(event) => this.handleColorChange(event, i)}
+//           />
+//         ))}
+//       </StyledMain>
+//     )
+//   }
+// }
 
 export default App;
--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,24 @@
 import { render } from '@testing-library/react';
-import App from './App';
+import App, {generateBackgroundCSS} from './App';
 
-test('renders learn react link', () => {
+
+test('renders app', () => {
   const app = render(<App />)
   expect(app).toBeTruthy()
 });
+
+test('generateBackgroundCSS generates correct"')
+
+describe("generateBackgroundCSS creates correct CSS string", () => {
+
+  test("it handles two colors", () => {
+    const colors = ["#000000", "#ab4378"]
+    const output = "background: linear-gradient(#000000,#ab4378);"
+    expect(generateBackgroundCSS(colors)).toBe(output)
+  })
+  test("it handles multiple colors", () => {
+    const colors = ["#000000", "#ab3498", "#bb5e3e"]
+    const output = "background: linear-gradient(#000000,#ab4378,#bb5e3e);"
+    expect(generateBackgroundCSS(colors)).toBe(output)
+  })
+})
